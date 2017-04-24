d72e1b3953919fe8b40624101c018e476b2f6eea
JulienSimplonSarcelles <julien@grach.fr>
2017-04-24 14:57:47 +0200

    API liée et Json récupéré

--- app/meteoAccueilCtrl.js
+++ app/meteoAccueilCtrl.js
@@ -1,3 +1,17 @@
-app.controller('meteoAccueilCtrl', function($scope){
-  
-});
+app.controller('meteoAccueilCtrl', ['$scope', '$http', function ($scope, $http) {
+
+        $scope.chercher = function () {
+
+            $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=71ccfac7abcfa4eceae01a46379e4422',).then(function (response) {
+                $scope.meteo = response.data;
+            });
+        }
+
+
+
+
+    }]
+);
+
+
+
