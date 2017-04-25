60b24c3fbfebdd630ed4eed7df9a1ad06aa481f9
JulienSimplonSarcelles <julien@grach.fr>
2017-04-25 17:10:44 +0200

    Json affiché

--- app/meteoAccueilCtrl.js
+++ app/meteoAccueilCtrl.js
@@ -1,12 +1,25 @@
 app.controller('meteoAccueilCtrl', ['$scope', '$http', function ($scope, $http) {
 
+        $scope.i = 0;
+
         $scope.chercher = function () {
 
-            $http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=71ccfac7abcfa4eceae01a46379e4422',).then(function (response) {
+            $http.get('http://api.openweathermap.org/data/2.5/forecast?q='+$scope.cityUser+'&units=metric&appid=71ccfac7abcfa4eceae01a46379e4422').then(function (response) {
                 $scope.meteo = response.data;
             });
-        }
+        };
 
+        $scope.today = function () {
+            $scope.i = 0;
+        };
+
+        $scope.tomorrow = function () {
+            $scope.i = 8;
+         };
+
+        $scope.afterTomorrow = function () {
+            $scope.i = 16;
+        };
 
 
 
